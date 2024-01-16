8f417496644ec07e8b70fb47ebc0103ca5200056
Carlos Martel <102035692+CarlosMA17@users.noreply.github.com>
2024-01-16 08:35:23 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './Header';
-import RegisterScreen from './screens/RegisterScreen';
+
 
 export default function App() {
   return (
     <>
-      <Header title={'App practica'}></Header>
       <View style={styles.container}>
-        <RegisterScreen />
+        <Text>Hola mundo</Text>
       </View>
     </>
   );
